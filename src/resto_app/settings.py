bbc70e1a06e1021f20b5264ed160fbf64beac656
Cristobal Barberis <cristobal@barberis.com>
2022-10-12 23:50:55 -0400

    collect static fix

--- src/resto_app/settings.py
+++ src/resto_app/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "staticfiles/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-    '/var/www/staticfiles/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles",
+#     '/var/www/staticfiles/',
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
