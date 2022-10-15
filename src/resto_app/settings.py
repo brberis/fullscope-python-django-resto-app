39f5faaaa822aa503c38fdc0216936d9d635746f
Cristobal Barberis <cristobal@barberis.com>
2022-10-15 01:08:10 -0400

    css debug

--- src/resto_app/settings.py
+++ src/resto_app/settings.py
@@ -141,10 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 STATIC_URL = "static/"
- # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# USE IN LOCAL
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
