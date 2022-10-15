f6b4958e6f5624003c488a0257d22ff23405c5b9
Cristobal Barberis <cristobal@barberis.com>
2022-10-15 02:54:42 -0400

    css

--- src/resto_app/settings.py
+++ src/resto_app/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG", "True") == "False"
 # DEBUG = (sys.argv[1] == 'runserver')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
