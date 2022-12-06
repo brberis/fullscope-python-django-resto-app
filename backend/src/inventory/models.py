cb2826e12039239eec7b7361054389db7f8a9470
Cristobal Barberis <cristobal@barberis.com>
2022-12-05 21:11:55 -0500

    products app

--- backend/src/inventory/models.py
+++ backend/src/inventory/models.py
@@ -1,58 +1,5 @@
 from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name=('Name'))
-    description = models.TextField(verbose_name=('Description'), null=True, blank=True)
-    order = models.PositiveSmallIntegerField(('Order'), default=99, null=True)
-
-
-    def __str__(self):
-        return self.name 
-
-    class Meta:
-        verbose_name = ('Category')
-        verbose_name_plural = ('Categories')
-        ordering = ['order']
-
-class ItemManager(models.Model):
-    def active(self, **kwargs):
-        return self.filter(active=True, **kwargs)
-    def featured(self, **kwargs):
-        return self.filter(is_featured=True, **kwargs)
-
-        
-class Item(models.Model):
-    title=models.CharField(('Name'), max_length=250)
-    item_description=models.TextField(
-        verbose_name=('Item Description'), default='',
-        blank=True
-    )
-    price = models.DecimalField(default=0, max_digits=12, decimal_places=0, verbose_name=('Price'))
-    categories = models.ManyToManyField(Category,
-                                         verbose_name=('Categories'),
-                                         blank=True)
-    active = models.BooleanField(('Active'), default=False,
-                                       db_index=True)
-    is_featured = models.BooleanField(('Featured'), default=False,
-                                      db_index=True)
-    featured_image = models.CharField(
-        verbose_name=('Featured image'),
-        null=True,
-        blank=True,
-        max_length=150)
-    for_sale = models.BooleanField(verbose_name=('For Sale'), default=False)
-
-    order = models.PositiveSmallIntegerField(('Order'), default=99, null=True)
-    objects = ItemManager()
-
-    def __str__(self):
-        return self.title 
-
-    class Meta:
-        ordering = ['order']
-        verbose_name = ('Product')
-        verbose_name_plural = ('Products')
+from products.models import Item
 
 class Location(models.Model):
 
--- backend/src/resto_app/settings.py
+++ backend/src/resto_app/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'resto_app',
+    'products',
     'inventory',
     'contacts',
     'signpage',
