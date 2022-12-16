fadedcafdd78a77d5220087d2e3c05fdef3b7c40
Cristobal Barberis <cristobal@barberis.com>
2022-12-16 14:57:34 -0500

    frontend from the beginning using next

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import SpotEvents from "../components/spotEvents"
+
+export default function Home() {
+  return (
+    <div className="container mx-auto my-10">
+      <SpotEvents />
+    </div>
+  )
+}
---
+++ frontend/pages/test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+  
+const Test = () => {
+  return (
+    <div>This is a dummy page</div>
+  )
+}
+  
+export default Test
