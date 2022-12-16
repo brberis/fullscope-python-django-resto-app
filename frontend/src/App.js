c9c49fdf369866b86fdcb8680290f20b969357e3
Cristobal Barberis <cristobal@barberis.com>
2022-12-15 22:59:41 -0500

    navbar component and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
 
 
 function App() {
 
-  return (
-   
-      <div>
 
-      </div>
+  return (
+    <div>
+      <Navbar />
+    </div>
   );
-}
+};
 
 export default App;
+
