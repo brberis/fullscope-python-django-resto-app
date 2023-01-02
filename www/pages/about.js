9851983107ade4a1e4c09ef82fd488bb10783c48
Cristobal Barberis <cristobal@barberis.com>
2023-01-01 23:46:50 -0500

    about ts to js

--- www/pages/about.js
+++ www/pages/about.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 
-const About = (): React.ReactElement => {
+export default function About()
   return (
     <Layout>
       <h1 className="text-xl pt-3 pb-5">About page goes here</h1>
@@ -8,4 +8,3 @@ const About = (): React.ReactElement => {
   )
 }
 
-export default About;
