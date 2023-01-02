96f0ce59fa39509bf0633c580b0ee40db4e41326
Cristobal Barberis <cristobal@barberis.com>
2023-01-01 23:48:07 -0500

    about ts to js

--- www/pages/about.js
+++ www/pages/about.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 
-export default function About()
+export default function About() {
   return (
     <Layout>
       <h1 className="text-xl pt-3 pb-5">About page goes here</h1>
